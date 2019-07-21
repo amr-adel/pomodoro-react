1970ae5a45dd2603ba8a8049ff79b6837b8b19d4
Amr A. Abdelmoez <amradel88@live.com>
2019-07-21 16:59:20 +0200

    feat: Layout essentials

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react'
 
 function App() {
-  return <div className='App' />
+  return <div>
+    <div className="main">
+      <div className="inner"></div>
+    </div>
+  </div>
 }
 
 export default App
