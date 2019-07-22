3f310a6274f8073d6a3a1ca2372106f77d4f8528
Amr A. Abdelmoez <amradel88@live.com>
2019-07-22 20:32:51 +0200

    feat: Add controlLength function to reducer

--- src/reducers/timerReducer.js
+++ src/reducers/timerReducer.js
@@ -1,9 +1,27 @@
+const controlLength = (state, label, operation) => {
+  let toModify = `${label}Length`
+
+  if (!state.active) {
+    if (operation === 'increment' && state[toModify] < 60) {
+      return { ...state, [toModify]: state[toModify] + 1 }
+    } else if (operation === 'decrement' && state[toModify] > 1) {
+      return { ...state, [toModify]: state[toModify] - 1 }
+    }
+  }
+
+  return state
+}
+
 export const timerReducer = (state, action) => {
   switch (action.type) {
     case 'SESSION_INC':
-      return { ...state, sessionLength: state.sessionLength + 1 }
+      return controlLength(state, 'session', 'increment')
     case 'SESSION_DEC':
-      return { ...state, sessionLength: state.sessionLength - 1 }
+      return controlLength(state, 'session', 'decrement')
+    case 'BREAK_INC':
+      return controlLength(state, 'break', 'increment')
+    case 'BREAK_DEC':
+      return controlLength(state, 'break', 'decrement')
     default:
       return state
   }
