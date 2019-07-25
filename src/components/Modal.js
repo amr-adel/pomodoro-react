883cd9230d1dfa702ee638c3ffb54dcd321f8dfe
Amr A. Abdelmoez <amradel88@live.com>
2019-07-25 04:15:40 +0200

    feat: Add modal show/hide animations

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,37 +1,14 @@
 import React, { Component } from 'react'
 import { createPortal } from 'react-dom'
+import './Modal.scss'
 
 const modalRoot = document.getElementById('modal')
-
-const modalBg = {
-  position: 'fixed',
-  width: '100%',
-  height: '100vh',
-  top: '0',
-  left: '0',
-  backgroundColor: 'rgba(4, 4, 4, 0.8)',
-  transition: 'all 0.3s ease-out',
-  overflow: 'hidden',
-  zIndex: '10',
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center'
-}
-
-const modalContainer = {
-  backgroundColor: '#f2f2f2',
-  padding: '4rem',
-  width: '70vw',
-  maxHeight: '80vh',
-  borderRadius: '3px',
-  overflow: 'auto'
-}
 
 class Portal extends Component {
   render() {
     return createPortal(
-      <div style={modalBg} onClick={this.props.hideModal}>
-        <div style={modalContainer} onClick={e => e.stopPropagation()}>
+      <div className='modal' onClick={this.props.hideModal}>
+        <div className='modal__container' onClick={e => e.stopPropagation()}>
           {this.props.children}
         </div>
       </div>,
@@ -43,7 +20,16 @@ class Portal extends Component {
 class Modal extends Component {
   state = { showModal: false }
 
-  handleToggle = () => this.setState({ showModal: !this.state.showModal })
+  handleToggle = () => {
+    const modal = document.querySelector('.modal')
+    
+    if (modal !== null) {
+      modal.style.animation = 'zoom-reverse .3s ease-out'
+      setTimeout(() => this.setState({ showModal: !this.state.showModal }), 300)
+    } else {
+      this.setState({ showModal: !this.state.showModal })
+    }
+  }
 
   render() {
     return (
