537ff4127f3ead69a46700a5a19a37e29722aaf4
Amr A. Abdelmoez <amradel88@live.com>
2019-07-25 23:17:39 +0200

    Update "about" section content

--- src/components/ModalContent.js
+++ src/components/ModalContent.js
@@ -13,11 +13,31 @@ const ModalContent = () => {
             target='_blank'>
             FreeCodeCamp
           </a>{' '}
-          Front End Libraries Certification, which requires using a frontend framework (like React for example) to build
-          a Pomodoro Clock. the project must fulfill all the user stories and get all the tests passed.
+          Front End Libraries Certification, which requires using a frontend framework to build a Pomodoro Clock. the
+          project must fulfill all the user stories and get all the tests passed.
         </p>
-        
-        <p className='text'>In this project, I'm using</p>
+        <p className='text'>
+          In this project, I'm using{' '}
+          <a href='https://reactjs.org/' target='_blank'>
+            React
+          </a>{' '}
+          with its new{' '}
+          <a href='https://reactjs.org/docs/context.html' target='_blank'>
+            Context API
+          </a>{' '}
+          and{' '}
+          <a href='https://reactjs.org/docs/hooks-intro.html' target='_blank'>
+            Hooks
+          </a>{' '}
+          to build a functional Pomodoro clock.
+        </p>
+        <p className='text'>
+          {' '}
+          <a href='https://reactjs.org/docs/portals.html' target='_blank'>
+            Portal
+          </a>{' '}
+          was used too, to create this beautiful modal.{' '}
+        </p>
         <p className='text'>
           Interested in code? Check the project's repository on{' '}
           <a href='https://github.com/amr-adel/pomodoro-react' target='_blank'>
@@ -34,42 +54,42 @@ const ModalContent = () => {
         </p>
         <ul className='social'>
           <li>
-            <a href='https://www.fullstackamr.com' target='_blank' title="Portfolio">
+            <a href='https://www.fullstackamr.com' target='_blank' title='Portfolio'>
               <svg>
                 <use href={`${icons}#globe`} />
               </svg>
             </a>
           </li>
           <li>
-            <a href='https://twitter.com/fullstackamr' target='_blank' title="Twitter">
+            <a href='https://twitter.com/fullstackamr' target='_blank' title='Twitter'>
               <svg>
                 <use href={`${icons}#twitter`} />
               </svg>
             </a>
           </li>
           <li>
-            <a href='https://www.linkedin.com/in/amr-abdelmoez/' target='_blank' title="LinkedIn">
+            <a href='https://www.linkedin.com/in/amr-abdelmoez/' target='_blank' title='LinkedIn'>
               <svg>
                 <use href={`${icons}#linkedin`} />
               </svg>
             </a>
           </li>
           <li>
-            <a href='https://github.com/amr-adel' target='_blank' title="GitHub">
+            <a href='https://github.com/amr-adel' target='_blank' title='GitHub'>
               <svg>
                 <use href={`${icons}#github`} />
               </svg>
             </a>
           </li>
           <li>
-            <a href='https://codepen.io/amr-adel/' target='_blank' title="CodePen">
+            <a href='https://codepen.io/amr-adel/' target='_blank' title='CodePen'>
               <svg>
                 <use href={`${icons}#codepen`} />
               </svg>
             </a>
           </li>
           <li>
-            <a href='https://www.freecodecamp.org/amr-adel' target='_blank' title="FreeCodeCamp">
+            <a href='https://www.freecodecamp.org/amr-adel' target='_blank' title='FreeCodeCamp'>
               <svg>
                 <use href={`${icons}#freecodecamp`} />
               </svg>
