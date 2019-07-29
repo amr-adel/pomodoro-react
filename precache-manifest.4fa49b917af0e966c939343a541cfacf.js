7a5ead4df481891d26d748fbf9bf02735e4d396c
Amr A. Abdelmoez <amradel88@live.com>
2019-07-29 23:03:22 +0200

    Updates

---
+++ precache-manifest.4fa49b917af0e966c939343a541cfacf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e99082bd197351481efb7d625685b54",
+    "url": "/pomodoro-react/index.html"
+  },
+  {
+    "revision": "8e9e5ae36b3c7e04d2bf",
+    "url": "/pomodoro-react/static/css/main.f4c0a6c9.chunk.css"
+  },
+  {
+    "revision": "ab40c9d112093682a26f",
+    "url": "/pomodoro-react/static/js/2.8ec88d02.chunk.js"
+  },
+  {
+    "revision": "8e9e5ae36b3c7e04d2bf",
+    "url": "/pomodoro-react/static/js/main.a358f8a1.chunk.js"
+  },
+  {
+    "revision": "e8d36cf3d72ad2a16867",
+    "url": "/pomodoro-react/static/js/runtime~main.31035ebe.js"
+  },
+  {
+    "revision": "7b1bbee4889ce4e5298f9ff6171dc807",
+    "url": "/pomodoro-react/static/media/icons.7b1bbee4.svg"
+  }
+]);
